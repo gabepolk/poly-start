ffc44a6f40e576895f63609eb45188c5d9ad7daa
Gabe Polk <gabe.a.polk@gmail.com>
2014-07-23 10:54:23 -0700

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,43 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([
+  {
+    name: "Taylor Smith",
+    email: "taylor@themakersquare.com"
+  },
+  {
+    name: "Mike Ornellas",
+    email: "mike@themakersquare.com"
+  }
+  ])
+
+Video.create([
+  {
+    title: "Gladiator",
+    description: "A great movie",
+    youtube_id: "rNdKBPcVGJI",
+    user_id: 1
+  },
+  {
+    title: "Pacific Rim",
+    description: "Giant robots fighting giant sea monsters",
+    youtube_id: "SSNU6t0pmkw",
+    user_id: 2
+  }
+  ])
+
+Sound.create([
+  {
+    title: "Files",
+    soundcloud_url: "https://soundcloud.com/makersquare/files"
+  },
+  {
+    title: "Sitcom Don",
+    soundcloud_url: "https://soundcloud.com/makersquare/sitcom-don"
+  }
+])
